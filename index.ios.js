8888b94182f6c0151544b826021f49a7fd70de3c
Yuriy Shefer <sheff@sheffmachine.com>
2016-03-21 15:28:20 -0700

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,11 @@
+//Create a react component
+
+var Weekdays = React.createClass({
+    render: function() {
+        return (
+            
+            )
+    }
+});
+
+//Show the react component on the screen
